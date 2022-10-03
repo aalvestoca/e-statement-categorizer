07810528fa296c4d20859f264b001695b4ab9c1c
alexbelloni <alexbelloni@outlook.com>
2022-10-03 14:22:06 -0400

    txt format

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ const transaction = require('./transaction');
 
 const date1 = ''//'12/07/2021';
 const date2 = ''//'01/10/2022';
-const postedTransactionsFilename = './pcbanking.ascii';
+const postedTransactionsFilename = './pcbanking.txt';//'./pcbanking.ascii';
 
 //Read an e-Statement file from ScotiaBank
 fs.readFile(postedTransactionsFilename, (err, data) => {
     const statementText = data.toString();
     const lines = statementText.split('\n');
-
     const transactionArr = [];
     lines.forEach(line => {
-        const trans = transaction.getTransaction(line);
-        console.log(trans);
-        if (trans && (!date1 || trans.date >= new Date(date1.concat(' 00:00:00')) && trans.date <= new Date(date2.concat(' 23:59:59')))) {
+        const trans = transaction.getTransaction(line, 'txt');
+        if (trans && (!date1 || (trans.date >= new Date(date1.concat(' 00:00:00')) && trans.date <= new Date(date2.concat(' 23:59:59'))))) {
             if (!trans.category) {
                 //Need to configure in categories.json
                 console.log('!!! No category: ', line);
             } else {
+                if(['other','commute'].includes(trans.category) ){
+
+                }
                 transactionArr.push(trans)
             }
         }
@@ -29,8 +30,8 @@ fs.readFile(postedTransactionsFilename, (err, data) => {
     const totals = category.getValuesByCategory(transactionArr);
     console.log('totals',totals);
 
-    const transactions = transactionArr.filter(t=>t.category === 'pharmacy');
-    console.log(transactions);
+    const transactions = transactionArr.filter(t=>['other','commute','delivery'].includes(t.category));
+    console.log(Array.from(new Set(transactions.map(t=>t.category + ' ' + t.name))));
 })
 
 
--- transaction.js
+++ transaction.js
@@ -1,19 +1,41 @@
 const category = require('./category');
 
-function getTransaction(data) {
-    const d = data.split(',');
-    if (d[1]) {
-        const origin = d[1].split('  ');
-        const value = parseFloat(parseFloat(d[2]).toFixed(2));
-        const name = origin[0].replace('"', '');
-        const place = category.getPlace(name);
-
-        return {
-            date: new Date(d[0]),
-            name: place.name,
-            aka: place.aka || place.name,
-            value,
-            category: category.getCategory(origin[0]),
+function getTransaction(data, format) {
+    if(format === 'txt'){
+        const d = data.split(' ');
+        if (d[1]) {
+            const nameValueArr = d.splice(5).join(' ').split(' ON ')
+            //'021 Aug 30 Aug 31 HARARI AND CHOCRON DENTIS TORONTO ON 765.50\r',
+            const value = parseFloat(parseFloat(nameValueArr[1]).toFixed(2));
+            const name = nameValueArr[0];
+            const place = category.getPlace(name);
+            if(!place.name){
+                console.log(name)
+            }
+    
+            return {
+                //date: new Date(d[0]),
+                name: place.name,
+                aka: place.aka || place.name,
+                value,
+                category: category.getCategory(name),
+            }
+        }
+    } else {
+        const d = data.split(',');
+        if (d[1]) {
+            const origin = d[1].split('  ');
+            const value = parseFloat(parseFloat(d[2]).toFixed(2));
+            const name = origin[0].replace('"', '');
+            const place = category.getPlace(name);
+    
+            return {
+                date: new Date(d[0]),
+                name: place.name,
+                aka: place.aka || place.name,
+                value,
+                category: category.getCategory(origin[0]),
+            }
         }
     }
     return null;
